13f6fe2e4b9c3c8034adb19c5dab936ae9d9791a
bachtiarrizaa <bachtiarriza87@gmail.com>
2024-08-18 16:16:00 +0700

    post category

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,4 +14,8 @@ class Post extends Model
 
     // gaboleh diisi
     protected $guarded = ['id'];
+
+    public function category() {
+        return $this -> belongsTo(Category::class);
+    }
 }
