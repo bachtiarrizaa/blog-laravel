12137047feec28c04caf11e6482fcc4dc81d4967
bachtiarrizaa <bachtiarriza87@gmail.com>
2024-08-19 21:13:21 +0700

    db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,9 +17,58 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        User::create([
+            'name' => 'Bachtiar Riza',
+            'email' => 'bachtiar@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Riza',
+            'email' => 'riza@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-propgramming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita dolores nisi minima amet cum omnis, culpa consequuntur vitae est nulla.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia nulla repudiandae explicabo dolore expedita laudantium illum perferendis, quia fugiat id nesciunt tenetur ipsa enim harum ea quis iste ipsum aspernatur? Aliquid ipsam, sunt voluptatibus consectetur officia mollitia atque exercitationem, aperiam, cumque iure beatae autem eligendi! Cumque doloremque fugit voluptates voluptatibus ipsam, ad est molestias cupiditate quia itaque modi hic sit, ratione id, pariatur similique asperiores natus dolorem quibusdam iure? Dicta dolores eos reiciendis cumque illo voluptatem necessitatibus ipsum quos impedit autem, rem rerum, hic sapiente. Ad iusto non ea minus nulla itaque? Fugiat eum nobis iure, natus accusamus sequi ipsam commodi tempore voluptate vero tenetur non sapiente illum officiis eligendi, dignissimos facere autem, molestiae magni! Consequuntur iste rerum explicabo corporis?',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita dolores nisi minima amet cum omnis, culpa consequuntur vitae est nulla.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia nulla repudiandae explicabo dolore expedita laudantium illum perferendis, quia fugiat id nesciunt tenetur ipsa enim harum ea quis iste ipsum aspernatur? Aliquid ipsam, sunt voluptatibus consectetur officia mollitia atque exercitationem, aperiam, cumque iure beatae autem eligendi! Cumque doloremque fugit voluptates voluptatibus ipsam, ad est molestias cupiditate quia itaque modi hic sit, ratione id, pariatur similique asperiores natus dolorem quibusdam iure? Dicta dolores eos reiciendis cumque illo voluptatem necessitatibus ipsum quos impedit autem, rem rerum, hic sapiente. Ad iusto non ea minus nulla itaque? Fugiat eum nobis iure, natus accusamus sequi ipsam commodi tempore voluptate vero tenetur non sapiente illum officiis eligendi, dignissimos facere autem, molestiae magni! Consequuntur iste rerum explicabo corporis?',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ketiga',
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita dolores nisi minima amet cum omnis, culpa consequuntur vitae est nulla.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia nulla repudiandae explicabo dolore expedita laudantium illum perferendis, quia fugiat id nesciunt tenetur ipsa enim harum ea quis iste ipsum aspernatur? Aliquid ipsam, sunt voluptatibus consectetur officia mollitia atque exercitationem, aperiam, cumque iure beatae autem eligendi! Cumque doloremque fugit voluptates voluptatibus ipsam, ad est molestias cupiditate quia itaque modi hic sit, ratione id, pariatur similique asperiores natus dolorem quibusdam iure? Dicta dolores eos reiciendis cumque illo voluptatem necessitatibus ipsum quos impedit autem, rem rerum, hic sapiente. Ad iusto non ea minus nulla itaque? Fugiat eum nobis iure, natus accusamus sequi ipsam commodi tempore voluptate vero tenetur non sapiente illum officiis eligendi, dignissimos facere autem, molestiae magni! Consequuntur iste rerum explicabo corporis?',
+            'category_id' => 2,
+            'user_id' => 2
         ]);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,11 +3,11 @@
 @section('container')
     <article>
         <h1 class="mb-5">{{ $post->title }}</h1>
-        <p>By. Bachtiar Riza in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+        <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
         {!! $post->body !!}
     </article>
 
-    <a href="/posts">Bach to blog</a>
+    <a href="/posts" class="d-block mt-3 text-decoration-none">Back to blog</a>
 
 @endsection
 
