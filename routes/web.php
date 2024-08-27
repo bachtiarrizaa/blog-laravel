dfbdca1bbeb246bbd73dc1c08363b6fac4b2ffaf
bachtiarrizaa <bachtiarriza87@gmail.com>
2024-08-27 14:27:42 +0700

    search * pagination

--- routes/web.php
+++ routes/web.php
@@ -9,19 +9,20 @@ use App\Http\Controllers\PostController;
 // menggunakan closure
 Route::get('/', function () {
     return view('home', [
-        "title" => "Home"
+        "title" => "Home",
+        'active' => 'home',
     ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
         "title" => "About",
+        'active' => 'about',
         "name" => "Bachtiar Riza Pratama",
         "email" => "bachtiarrizap@gmail.com",
         "image" => "profile.jpeg"
     ]);
 });
-
 
 // menggunakan controller
 Route::get('/posts', [PostController::class, 'index']);
@@ -36,17 +37,18 @@ Route::get('categories', function() {
     ]);
 });
 
-Route::get('/categories/{category:slug}', function(Category $category) {
-    return view('posts', [
-        'title' => "Post By Category: $category->name",
-        'active' => 'categories',
-        'posts' => $category->posts->load('category', 'author')
-    ]);
-});
+// Route::get('/categories/{category:slug}', function(Category $category) {
+//     return view('posts', [
+//         'title' => "Post By Category: $category->name",
+//         'active' => 'categories',
+//         'posts' => $category->posts->load('category', 'author')
+//     ]);
+// });
 
-Route::get('/authors/{author:username}', function(User $author) {
-    return view('posts', [
-        'title' => "Post By Author : $author->name",
-        'posts' => $author->posts->load('category', 'author')
-    ]);
-});
+// Route::get('/authors/{author:username}', function(User $author) {
+//     return view('posts', [
+//         'title' => "Post By Author : $author->name",
+//         'active' => 'posts',
+//         'posts' => $author->posts->load('category', 'author')
+//     ]);
+// });
